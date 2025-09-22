725ad2ae51c676ada92b4a58fd8f360bc66fc1db
Adnan-Akil <hypedbeats04@gmail.com>
2025-09-22 15:45:38 +0400

    setup front-end and tailwindcss

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <div className="bg-[rgba(240,240,220,1)] flex h-screen w-screen">
+        
+      </div>
+    </div>
+  )
+}
+
+export default Home
